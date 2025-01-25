64da3ddb847da207e55243cc02a42d34bc3488fd
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2025-01-25 19:02:42 +0530

    modules

---
+++ CSS- Cascade Style Sheets/Projects/Amazon resp/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./{html,js}",
+    "./src/{js,css,ts,tsx}",
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
